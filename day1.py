48e53872c41cc9a67d1b662786ae77d30a9575ad
Aliriza <alirizaanwar@gmail.com>
2019-07-22 10:33:17 +0700

    add if

--- day1.py
+++ day1.py
@@ -5,4 +5,19 @@
 
 
 
-print ('hello world') where py
+print ('hello world')
+
+alas = 20
+tinggi = 2
+luas = alas*tinggi / 2
+
+print (luas)
+
+
+#branding dengan if COndition
+if luas < 20 :
+    print('Kecil')
+elif luas >= 20 and luas <50 :
+    print('Menengah')   
+else:
+    print('Besar')
