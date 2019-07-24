fd3bdf274ac251bc264b396ff5b75463dadd9460
Aliriza <alirizaanwar@gmail.com>
2019-07-24 13:20:17 +0700

    init commit

---
+++ Day1Django/virtuenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/riza/Documents/Training/Py-Odoo-ML-Training/Day1Django/virtuenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
